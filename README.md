56f7337f5dd96c4b3658fd4e94626d2443bb3f55
Pavel Hübner <pavel.hubner@gmail.com>
2018-12-07 22:07:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather Pump
+
+Weather Pump is a quick and dirty Python app for fetching data from OpenWeatherMap API service and passing the result to MQTT.
+
+## Contributing
+
+Please read [**CONTRIBUTING.md**](https://github.com/hubpav/sunrice/blob/master/CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [**SemVer**](https://semver.org/) for versioning. For the versions available, see the [**tags on this repository**](https://github.com/hubpav/weatherpump/tags).
+
+## Authors
+
+* [**Pavel Hübner**](https://github.com/hubpav) - Initial work
+
+## License
+
+This project is licensed under the [**MIT License**](https://opensource.org/licenses/MIT/) - see the [**LICENSE**](https://github.com/hubpav/sunrice/blob/master/LICENSE) file for details.
---
+++ weatherpump/__init__.py
@@ -0,0 +1,57 @@
+'OpenWeatherMap to MQTT bridge'
+
+import click
+import json
+import logging
+import paho.mqtt.client
+import requests
+import schedule
+
+__version__ = '1.0.0'
+
+logging.basicConfig(format='%(asctime)s <%(levelname)s> %(message)s',
+                    level=logging.DEBUG, datefmt="%Y-%m-%dT%H:%M:%S")
+
+
+def job_fetch_weather(key, city, topic):
+    logging.debug('Starting scheduled job')
+    try:
+        r = requests.get('https://api.openweathermap.org/data/2.5/weather?id=%s&units=metric&APPID=%s' % (city, key))
+        data = r.json()
+    except KeyboardInterrupt:
+        raise
+    except:
+        logging.error('Failed to fetch data from OpenWeatherMap')
+    try:
+        client.publish(topic, qos=1, payload=data)
+    except KeyboardInterrupt:
+        raise
+    except:
+        logging.error('Failed to publish the data')
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info('MQTT connected (code: %d)' % rc)
+
+
+@click.command()
+@click.option('--host', '-h', default='127.0.0.1', help='MQTT broker host.')
+@click.option('--port', '-p', default='1883', help='MQTT broker port.')
+@click.option('--key', '-k', required=True, help='OpenWeatherMap API key.')
+@click.option('--city', '-c', required=True, help='OpenWeatherMap city id.')
+@click.option('--topic', '-t', required=True, help='MQTT topic to used for publishing.')
+@click.version_option(version=__version__)
+def main(host, port, key, city, topic):
+    try:
+        logging.info('Program started')
+        logging.getLogger('schedule').propagate = False
+        logging.getLogger('urllib3').propagate = False
+        mqtt = paho.mqtt.client.Client()
+        mqtt.on_connect = on_connect
+        mqtt.connect(host, int(port))
+        schedule.every(10).seconds.do(job_fetch_weather, key, city, topic)
+        while True:
+            schedule.run_pending()
+            mqtt.loop()
+    except KeyboardInterrupt:
+        pass

323df3790375913e42878d2e2e05d149289d93a1
Pavel Hübner <pavel.hubner@gmail.com>
2018-12-07 22:18:52 +0100

    Fix publish

--- weatherpump/__init__.py
+++ weatherpump/__init__.py
@@ -10,10 +10,10 @@ import schedule
 __version__ = '1.0.0'
 
 logging.basicConfig(format='%(asctime)s <%(levelname)s> %(message)s',
-                    level=logging.DEBUG, datefmt="%Y-%m-%dT%H:%M:%S")
+                    level=logging.DEBUG, datefmt='%Y-%m-%dT%H:%M:%S')
 
 
-def job_fetch_weather(key, city, topic):
+def job_fetch_weather(mqtt, key, city, topic):
     logging.debug('Starting scheduled job')
     try:
         r = requests.get('https://api.openweathermap.org/data/2.5/weather?id=%s&units=metric&APPID=%s' % (city, key))
@@ -23,7 +23,7 @@ def job_fetch_weather(key, city, topic):
     except:
         logging.error('Failed to fetch data from OpenWeatherMap')
     try:
-        client.publish(topic, qos=1, payload=data)
+        mqtt.publish(topic, qos=1, payload=data)
     except KeyboardInterrupt:
         raise
     except:
@@ -50,7 +50,7 @@ def main(host, port, key, city, topic, interval):
         mqtt = paho.mqtt.client.Client()
         mqtt.on_connect = on_connect
         mqtt.connect(host, int(port))
-        schedule.every(interval).minutes.do(job_fetch_weather, key, city, topic)
+        schedule.every(interval).minutes.do(job_fetch_weather, mqtt, key, city, topic)
         while True:
             schedule.run_pending()
             mqtt.loop()
